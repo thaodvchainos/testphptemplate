d61337566fd264369dbfc57918a0233165489c2e
thaodvchainos <46013736+thaodvchainos@users.noreply.github.com>
2019-01-07 22:20:40 -0500

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends Illuminate\Foundation\Testing\TestCase
+class ExampleTest extends TestCase
 {
     /**
      * A basic functional test example.
